8874c6967e490be0765390fbe8a48ae62b979272
Justsnoopy30 <justsnoopy30@hypercubemc.tk>
2020-08-01 12:55:11 -0500

    Improve BeaconPluginInstance

--- src/main/java/net/hypercubemc/beacon/BeaconPluginInstance.java
+++ src/main/java/net/hypercubemc/beacon/BeaconPluginInstance.java
@@ -1,11 +1,27 @@
 package net.hypercubemc.beacon;
 
+import net.hypercubemc.beacon.BeaconPluginState;
+
 public class BeaconPluginInstance {
     private String pluginName;
     private String pluginVersion;
+    private BeaconPluginState pluginState;
 
-    public BeaconPluginInstance(String pluginName, String pluginVersion) {
+    public BeaconPluginInstance(String pluginName, String pluginVersion, BeaconPluginState pluginState) {
         this.pluginName = pluginName;
         this.pluginVersion = pluginVersion;
+        this.pluginState = pluginState;
+    }
+
+    public String getPluginName() {
+        return pluginName;
+    }
+
+    public String getPluginVersion() {
+        return pluginVersion;
+    }
+
+    public BeaconPluginState getPluginState() {
+        return BeaconPluginState;
     }
 }
