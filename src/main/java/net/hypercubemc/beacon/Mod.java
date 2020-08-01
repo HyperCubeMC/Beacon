a99a28b8dedd15ff33093041841d589654493d69
Justsnoopy30 <justsnoopy30@hypercubemc.tk>
2020-08-01 10:40:41 -0500

    Fix some indentation

--- src/main/java/net/hypercubemc/beacon/Mod.java
+++ src/main/java/net/hypercubemc/beacon/Mod.java
@@ -52,7 +52,7 @@ public class Mod implements ModInitializer {
 		});
 	}
 
-    public void setupTriggerCommandAliases() {
+        public void setupTriggerCommandAliases() {
 		Logger log = LogManager.getLogger("beacon");
 		ServerStartCallback.EVENT.register(server -> {
 //			for (String usage : server.getCommandManager().getDispatcher().getAllUsage(server.getCommandManager().getDispatcher().getRoot().getChild("trigger"), server.getCommandSource(), false)) {
@@ -98,7 +98,7 @@ public class Mod implements ModInitializer {
 				}
 			}
 		});
-    }
+        }
 
 	@Override
 	public void onInitialize() {
