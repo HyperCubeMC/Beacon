b2e883551615acedcd5d73591a14f8da7a9d4c76
Justsnoopy30 <justsnoopy30@hypercubemc.tk>
2020-08-01 12:38:24 -0500

    Create BeaconPluginInstance.java

---
+++ src/main/java/net/hypercubemc/beacon/BeaconPluginInstance.java
@@ -0,0 +1,11 @@
+package net.hypercubemc.beacon;
+
+public class BeaconPluginInstance {
+    private String pluginName;
+    private String pluginVersion;
+
+    public BeaconPluginInstance(String pluginName, String pluginVersion) {
+        this.pluginName = pluginName;
+        this.pluginVersion = pluginVersion;
+    }
+}
