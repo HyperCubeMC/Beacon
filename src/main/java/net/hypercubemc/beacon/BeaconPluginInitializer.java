7be8786a73320d4b8b1c0df3af551c746e62cba0
Justsnoopy30 <justsnoopy30@hypercubemc.tk>
2020-08-04 08:51:47 -0500

    Add javadoc to BeaconPluginInitializer

--- src/main/java/net/hypercubemc/beacon/BeaconPluginInitializer.java
+++ src/main/java/net/hypercubemc/beacon/BeaconPluginInitializer.java
@@ -1,5 +1,9 @@
 package net.hypercubemc.beacon;
 
+/**
+ * Implement this to make your plugin initializer class
+ * Then use BeaconPluginManager.registerPlugin(YourBeaconPluginInitializer) in your mod class or elsewhere to register it 
+ */
 public interface BeaconPluginInitializer {
     String getPluginName();
     String getPluginVersion();
