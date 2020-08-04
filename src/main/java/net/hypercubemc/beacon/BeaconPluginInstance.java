8ec6646350d0db1fa2f8ddf13f496054c1eb99e3
Justsnoopy30 <justsnoopy30@hypercubemc.tk>
2020-08-04 08:58:05 -0500

    Add javadoc a to BeaconPluginInstance

--- src/main/java/net/hypercubemc/beacon/BeaconPluginInstance.java
+++ src/main/java/net/hypercubemc/beacon/BeaconPluginInstance.java
@@ -5,6 +5,9 @@ import net.hypercubemc.beacon.BeaconPluginLogger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * Represents an instance of a Beacon plugin
+ */
 public class BeaconPluginInstance {
     private final String pluginName;
     private final String pluginVersion;
@@ -15,15 +18,27 @@ public class BeaconPluginInstance {
         this.pluginVersion = pluginVersion;
         this.pluginState = pluginState;
     }
-
+    
+    /**
+     * Gets the name of the plugin
+     * @return The name of the plugin as a String
+     */
     public String getPluginName() {
         return pluginName;
     }
 
+    /**
+     * Gets the version of the plugin
+     * @return The version of the plugin as a String
+     */
     public String getPluginVersion() {
         return pluginVersion;
     }
 
+    /**
+     * Gets the state of the plugin
+     * @return The state of the plugin as a BeaconPluginState
+     */
     public BeaconPluginState getPluginState() {
         return pluginState;
     }
@@ -31,11 +46,20 @@ public class BeaconPluginInstance {
     void setPluginState(BeaconPluginState pluginState) {
         this.pluginState = pluginState;
     }
-
+    
+    /**
+     * Gets the direct log4j2 logger for the plugin
+     * @return A Logger
+     */
     public Logger getRawLogger() {
         return LogManager.getLogger(pluginName);
     }
-
+    
+    /**
+     * Gets the BeaconPluginLogger for the plugin
+     * This is what you should be using to log
+     * @return A BeaconPluginLogger
+     */
     public BeaconPluginLogger getLogger() {
         return new BeaconPluginLogger(pluginName);
     }
