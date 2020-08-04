65b3a36ada79739b976b06d27359bbb2c23574bf
Justsnoopy30 <justsnoopy30@hypercubemc.tk>
2020-08-04 17:35:10 -0500

    Fix player death event.

--- src/main/java/net/hypercubemc/beacon/mixin/LivingPlayerMixin.java
+++ src/main/java/net/hypercubemc/beacon/mixin/LivingPlayerMixin.java
@@ -1,14 +1,14 @@
 package net.hypercubemc.beacon.mixin;
 
 import net.hypercubemc.beacon.api.events.BeaconPlayerDeathEvent;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LivingEntity.class)
+@Mixin(PlayerEntity.class)
 public abstract class LivingPlayerMixin {
     @Inject(method = "onDeath", at = @At(value = "HEAD"))
     public void prePlayerDeath(DamageSource source, CallbackInfo callbackInfo) {
