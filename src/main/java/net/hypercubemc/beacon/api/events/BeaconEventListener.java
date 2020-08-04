a8c791e2e487352132674b876ab0cba486ebf3c6
Justsnoopy30 <justsnoopy30@hypercubemc.tk>
2020-08-04 09:12:20 -0500

    Add javadoc to BeaconEventListener

--- src/main/java/net/hypercubemc/beacon/api/events/BeaconEventListener.java
+++ src/main/java/net/hypercubemc/beacon/api/events/BeaconEventListener.java
@@ -1,5 +1,12 @@
 package net.hypercubemc.beacon.api.events;
 
+/**
+ * This is how you add an event listener class
+ * First, implement this class
+ * Then, start adding event handlers by annotating methods with @BeaconEventHandle
+ * Specify the event in the value of the annotation
+ * When finished, in your main class or elsewhere, use {@link net.hypercubemc.beacon.api.events.BeaconEventManager#registerListener BeaconEventManager.registerPlugin(YourBeaconEventListener)} to register your event listener class and your event handlers
+*/
 public interface BeaconEventListener {
 
 }
